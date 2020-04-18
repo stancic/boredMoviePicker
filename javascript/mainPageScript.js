09f601efd7bf6b05d9d3d4349ef9c4a9e95ca43b
Dino Stancic <dinostancic7@gmail.com>
2020-04-18 11:36:24 +0200

    Functionality for movie page is done, now menu

--- javascript/mainPageScript.js
+++ javascript/mainPageScript.js
@@ -1,25 +1,33 @@
 document.querySelector(".pick-page").style.opacity="0";
 document.querySelector(".next-movie").style.display="none";
 let genre = document.querySelector(".select");
+let lowerGenre = document.querySelector(".lower-select")
 let year = document.querySelector('input[name="year"]');
+let lowerYear = document.querySelector('input[name="year-lower"]');
 let button = document.querySelector(".button");
 let movieHeader = document.querySelector(".header");
 let movieParagraph = document.querySelector(".about")
+let trailer = document.querySelector(".trailer-link");
+let imdb = document.querySelector(".imdb-link");
+let heart = document.querySelector("#heart"); 
 //ITEMS ON PAGE
 
+let hasClicked = false;
 
 //API AND MOVIE VARIABLES
 let genreUrl = 'https://api.themoviedb.org/3/genre/movie/list';//API
 let key = '7be00fe841740644dc3f1b6a0bd11fa7';                  //API
 let moviesUrl = 'https://api.themoviedb.org/3/discover/movie'; //API
-let genreId, movieTitle, movieOverview, movieBackground;//MOVIE(PRINT ON SCREEN)
+let trailerUrl = 'https://api.themoviedb.org/3/movie/';        //API
+let externalIDUrl = 'https://api.themoviedb.org/3/movie/';        //API
+let genreId, movieTitle, movieOverview, movieBackground, movieTrailer, movieId;//MOVIE(PRINT ON SCREEN)
+let imdbId;
 
 //JQUERY
 setTimeout(function(){
     $(".title").animate({left: "-50%"},1000);
     $(".pick-page").animate({opacity: "0"},600);
     $(".pick-page").animate({opacity: "1"},1000);
-    
 },1000);
 
 
@@ -27,8 +35,17 @@ $(".button").click(function(){
     $(".pick-page").css("display","none");
     $(".movie-content").slideToggle();
     $(".next-movie").css("display","inline-block");
+    apiCall(); 
+    lowerGenre.value = genre.value;
+    lowerYear.value = year.value;
+});
+
+$(".lower-button").click(function(){
+    genre = lowerGenre;
+    year = lowerYear;
     apiCall();
 });
+
 
 function apiCall(){
     $.getJSON(genreUrl + "?api_key=" + key + "&language=en-US",function(data){
@@ -41,6 +58,15 @@ function apiCall(){
     });
 }
 
+$(heart).click(function(){
+    if(heart.innerHTML === "favorite_border"){
+        heart.innerHTML = "favorite";
+    }
+    else{
+        heart.innerHTML = "favorite_border"
+    }
+});
+
 
 function getData(){
     let randomPick = Math.floor(Math.random()*19);
@@ -49,18 +75,30 @@ function getData(){
         movieOverview = data.results[randomPick].overview;
         movieBackgroundURL = JSON.stringify(data.results[randomPick].backdrop_path).replace(/"/g,'');
         movieBackground = 'https://image.tmdb.org/t/p/original'  + movieBackgroundURL ;
-
+        movieId = data.results[randomPick].id;
         printOnScreen();
+        getTrailer();
+        getIMDBid();
+    });
+    
+}
+function getTrailer(){
+    $.getJSON(trailerUrl + JSON.stringify(movieId) + "/videos?api_key=" + key + "&language=en-US",function(data){
+        movieTrailer = data.results[0].key
+        $(trailer).attr("href","https://www.youtube.com/watch?v=" + movieTrailer);
         
-        console.log(data.results[randomPick]);
-        console.log(movieTitle);
-        console.log(movieOverview);
-        console.log(movieBackground);
     });
 }
 
+function getIMDBid(){
+    $.getJSON(externalIDUrl + JSON.stringify(movieId) + "external_ids?api_key=" + key,function(data){
+        imdbId = data.imdb_id;
+        $(imdb).attr("href","https://www.imdb.com/title/" + imdbId);
+    });
+}
 function printOnScreen(){
-    movieHeader.innerHTML = movieTitle + '<span style class="material-icons heart">favorite_border</span>';
+    
+    movieHeader.innerHTML = movieTitle;
     movieParagraph.innerHTML = movieOverview;
     $('body').css({
         "background" : "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)) , url(" + movieBackground +") no-repeat center center fixed",
@@ -69,6 +107,8 @@ function printOnScreen(){
     $(".movie-content").css({
         "display" : "box"
     });
+    genre = null;
+    year = null;
 }
 
 
